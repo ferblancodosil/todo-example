82338eac0d6a24e995fc47b68cafca9db5032a9e
blain <blain@argalladas.eu>
2021-10-28 11:59:00 +0200

    Proyecto base

---
+++ src/App.vue
@@ -0,0 +1,18 @@
+<script setup>
+import TodoList from '@/views/TodoList.vue'
+</script>
+
+<template>
+  <div></div>
+</template>
+
+<style>
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
